8d538c79a7559691300380c308b23d42617283be
Eshan Rajapakshe <89007586+eshanaventude@users.noreply.github.com>
2024-12-25 17:14:54 +0530

    Sticky header

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -4,25 +4,26 @@ import './styles.scss';
 import ProfilePicture from '../../../assets/images/profile-pic.png';
 import Logo from '../../../assets/images/logo.svg';
 
-
 export const Header = () => {
   return (
     <header className="header">
-      <div className="brand-wrapper">
-        <img src={Logo} alt="brand logo" className="logo-img" />
-      </div>
-
-      <div className="user-wrapper">
-        <div className="user-info">
-          <Typography variant="p" customClassName="user-name">
-            Hi, John Doe
-          </Typography>
-          <Typography variant="p" customClassName="user-greetings">
-            Welcome
-          </Typography>
+      <div className="header-wrapper">
+        <div className="brand-wrapper">
+          <img src={Logo} alt="brand logo" className="logo-img" />
         </div>
 
-        <CircularImageFrame frameSize='small' imageScource={ProfilePicture} />
+        <div className="user-wrapper">
+          <div className="user-info">
+            <Typography variant="p" customClassName="user-name">
+              Hi, John Doe
+            </Typography>
+            <Typography variant="p" customClassName="user-greetings">
+              Welcome
+            </Typography>
+          </div>
+
+          <CircularImageFrame frameSize="small" imageScource={ProfilePicture} />
+        </div>
       </div>
     </header>
   );
